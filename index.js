3cabeabf793f4ddc758d2eee4f016dc9597ff2cb
Jafar Miles <jafarmiles@gmail.com>
2017-05-24 01:06:37 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+var express = require("express");
+var app = express();
+
+/* Serve main webpage */
+app.get("/", function(req, res) {
+	res.sendFile('index.html');
+});
+
+var port = 8080;
+app.listen(port, function() {
+	console.log("Listening on port " + port);
+})
