ae09f82b9a23290ed120122b1d7ee14fce9c0ba5
Jafar Miles <jafarmiles@gmail.com>
2017-12-28 23:22:11 +0000

    creating add chatter page

--- index.js
+++ index.js
@@ -1,12 +1,42 @@
 var express = require("express");
 var app = express();
 
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({	extended: true })); // support encoded bodies
+
+var tweets = [];
+
+var preHtml = "<html><head></head><body><title>Chatter</title><h1>Chatter</h1><a href=\"\/AddChatter\">Add Chat</a><ul>"
+var postHtml = "</ul></body></html>"
+
 /* Serve main webpage */
-app.get("*", function(req, res) {
+app.get("/", function(req, res) {
+	var html = "";
+
+	for(i = 0; i < tweets.length; i++) {
+		html += "<li>" + tweets[i] + "</li>";
+	}
+
+	res.send(preHtml + html + postHtml);
+});
+
+app.get("/AddChatter", function(req, res) {
 	var options = {
 		root: __dirname
-	}
+	};
 	res.sendFile('index.html', options);
+});
+
+app.post("*", function(req, res){
+	tweets.push(req.body.text);
+
+	var html = "";
+
+	for(i = 0; i < tweets.length; i++) {
+		html += "<li>" + tweets[i] + "</li>";
+	}
+	res.redirect("/");
 });
 
 var port = 8080;
